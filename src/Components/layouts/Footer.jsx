ac7b83ae4a9eb812858b8bc390b0bf42fd47ecb9
Tazwer Shakil <tazwershakilshakil@gmail.com>
2026-01-21 23:25:01 +0600

    make footer more reuseable and responsive

--- src/Components/layouts/Footer.jsx
+++ src/Components/layouts/Footer.jsx
@@ -3,7 +3,7 @@ import { IoMailOutline } from "react-icons/io5";
 import { NavLink } from "react-router-dom";
 
 const Footer = () => {
-     const linkStyle ='hover:text-primary transition-all duration-300 hover:ml-1'
+     const linkStyle = 'hover:text-primary transition-all duration-300 hover:ml-1'
 
      const links = <>
           <li><a href="#" className={linkStyle}>Shop All</a></li>
@@ -21,10 +21,15 @@ const Footer = () => {
           <li><a href="#" className={linkStyle}>Winter Wear</a></li>
           <li><a href="#" className={linkStyle}>Drop Shoulder</a></li>
      </>
+     const socials = [
+          { icon: <FaFacebookF size={20} />, link: "https://www.facebook.com/" },
+          { icon: <FaInstagram size={20} />, link: "https://www.instagram.com/" },
+          { icon: <FaTiktok size={20} />, link: "https://www.tiktok.com/" },
+     ];
 
      return (
-          <footer className="bg-secondary text-neutral-400 py-14 px-8 border-t border-neutral-700/90">
-               <div className="max-w-10/12 mx-auto">
+          <footer className="bg-secondary text-neutral-400 py-14 px-4 md:px-8 border-t border-neutral-700/90">
+               <div className="md:max-w-[75%] mx-auto">
                     {/* Main Footer Content */}
                     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-12">
                          {/* Column 1: Logo & Info */}
@@ -39,15 +44,11 @@ const Footer = () => {
                                    <span className="text-primary font-semibold">Owner:</span> Mustafa Tazwer Shakil
                               </p>
                               <div className="flex gap-5 mt-4 text-accent/90">
-                                   <a href="https://www.facebook.com/" target="_blank" className="p-2.5 rounded-full hover:bg-primary bg-neutral-800 transition-all duration-300 hover:scale-105 hover:-translate-y-1">
-                                        <FaFacebookF size={20} />
-                                   </a>
-                                   <a href="https://www.instagram.com/" target="_blank" className="p-2.5 rounded-full hover:bg-primary bg-neutral-800 transition-all duration-300 hover:scale-105 hover:-translate-y-1">
-                                        <FaInstagram size={20} />
-                                   </a>
-                                   <a href="https://www.tiktok.com/" target="_blank" className="p-2.5 rounded-full hover:bg-primary bg-neutral-800 transition-all duration-300 hover:scale-105 hover:-translate-y-1">
-                                        <FaTiktok size={20} />
-                                   </a>
+                                   {socials?.map((social, ind) => (
+                                        <a key={ind} href={social.link} target="_blank" className="p-2.5 rounded-full hover:bg-primary bg-neutral-800 transition-all duration-300 hover:scale-105 hover:-translate-y-1">
+                                             {social.icon}
+                                        </a>
+                                   ))}
                               </div>
                          </div>
                          {/* Column 2: Quick Links */}
@@ -61,7 +62,7 @@ const Footer = () => {
                          <div>
                               <h3 className="text-accent md:text-xl bebas mb-5 tracking-wider">Categories</h3>
                               <ul className="flex flex-col gap-4 text-sm">
-                                  {links2}
+                                   {links2}
                               </ul>
                          </div>
                          {/* Column 4: Contact Us */}
