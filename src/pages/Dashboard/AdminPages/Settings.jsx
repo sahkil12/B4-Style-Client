17f2cd9f1c13707864d20f5f854d9f85bb5c17c2
Tazwer Shakil <tazwershakilshakil@gmail.com>
2026-03-13 21:26:08 +0600

    added setting page in admin pannel

--- src/pages/Dashboard/AdminPages/Settings.jsx
+++ src/pages/Dashboard/AdminPages/Settings.jsx
@@ -1,8 +1,14 @@
 
 const Settings = () => {
      return (
-          <div>
-               
+          <div className="flex-1 p-4 lg:p-8  min-h-screen text-accent">
+               {/* Header Section */}
+               <div className="flex flex-wrap justify-between items-center gap-4 mb-10">
+                    <div>
+                         <h1 className="text-3xl lg:text-4xl font-medium uppercase tracking-wider bebas mb-1">SETTINGS</h1>
+                         <p className="text-accent/60 text-sm lg:text-base"> Manage your account settings</p>
+                    </div>
+               </div>
           </div>
      );
 };
